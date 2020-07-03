bbdb8cbb9614f179ef5c152d07f847b1687c4689
RinaYamashita <2020m134@kuas.ac.jp>
2020-07-03 11:39:07 +0900

    Create "Hot Food Style" page

---
+++ app/src/main/java/com/example/project03/Hot_Food_style.java
@@ -0,0 +1,14 @@
+package com.example.project03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hot_Food_style extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hot__food_style);
+    }
+}
