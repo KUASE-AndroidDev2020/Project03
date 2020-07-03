18be9fa1b8c6da9217420a7f110774a5cabcc585
RinaYamashita <2020m134@kuas.ac.jp>
2020-07-03 11:59:34 +0900

    Create "Cold Food Style" page

---
+++ app/src/main/java/com/example/project03/cold_food_style.java
@@ -0,0 +1,14 @@
+package com.example.project03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cold_food_style extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cold_food_style);
+    }
+}
