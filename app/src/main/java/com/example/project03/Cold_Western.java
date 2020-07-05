27d6e750fe455f78bec95c7b3099a3a4b1b20892
RinaYamashita <2020m134@kuas.ac.jp>
2020-07-05 11:02:44 +0900

    Create "Cold Western" page

---
+++ app/src/main/java/com/example/project03/Cold_Western.java
@@ -0,0 +1,14 @@
+package com.example.project03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cold_Western extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cold_western);
+    }
+}
