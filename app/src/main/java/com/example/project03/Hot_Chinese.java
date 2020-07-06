71995e9c11ce932eea70f0f7499e2896b9b4587f
RinaYamashita <2020m134@kuas.ac.jp>
2020-07-06 09:23:06 +0900

    Create "Hot Chinese" page

---
+++ app/src/main/java/com/example/project03/Hot_Chinese.java
@@ -0,0 +1,14 @@
+package com.example.project03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hot_Chinese extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hot_chinese);
+    }
+}
