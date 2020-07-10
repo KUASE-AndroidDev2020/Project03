346d9de5dbbdb2a8a65d4dd315025a325bc208bb
Maiparu <2020m121@kuas.ac.jp>
2020-07-10 11:58:59 +0900

    Connect Hot Food Style to Hot 3

--- app/src/main/java/com/example/project03/HotFoodStyle.java
+++ app/src/main/java/com/example/project03/HotFoodStyle.java
@@ -2,7 +2,9 @@ package com.example.project03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HotFoodStyle extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class HotFoodStyle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hot_food_style);
+    }
+
+    public void goToHotJapaneseActivity(View view) {
+        Intent toHotJapaneseActivity = new Intent(this, HotJapaneseActivity.class);
+        startActivity(toHotJapaneseActivity);
+    }
+
+    public void goToHotWesternActivity(View view) {
+        Intent toHotWesternActivity = new Intent(this, HotWesternActivity.class);
+        startActivity(toHotWesternActivity);
+    }
+
+    public void goToHot_Chinese(View view) {
+        Intent toHot_Chinese = new Intent(this, Hot_Chinese.class);
+        startActivity(toHot_Chinese);
     }
 }
