64c30aa8221f98b19c33b1cbf59a8afef55fc75d
RinaYamashita <2020m134@kuas.ac.jp>
2020-07-10 11:27:11 +0900

    solve error4

--- app/src/main/java/com/example/project03/ColdFoodStyle.java
+++ app/src/main/java/com/example/project03/ColdFoodStyle.java
@@ -2,15 +2,13 @@ package com.example.project03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class HotOrColdActivity extends AppCompatActivity {
+public class ColdFoodStyle extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hot_or_cold);
+        setContentView(R.layout.activity_cold_food_style);
     }
 }
--- app/src/main/java/com/example/project03/HotFoodStyle.java
+++ app/src/main/java/com/example/project03/HotFoodStyle.java
@@ -2,30 +2,13 @@ package com.example.project03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class HotFoodStyle extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hot__food_style);
-    }
-
-    public void goToHotJapaneseActivity(View view) {
-        Intent toHotJapanese =new Intent(this, HotJapaneseActivity.class);
-        startActivity(toHotJapanese);
-    }
-
-    public void goToHotWesternActivity(View view) {
-        Intent toHotWestern =new Intent(this, HotWesternActivity.class);
-        startActivity(toHotWestern);
-    }
-
-    public void goToHot_Chinese(View view) {
-        Intent toHot_Chinese = new Intent(this,Hot_Chinese.class);
-        startActivity(toHot_Chinese);
+        setContentView(R.layout.activity_hot_food_style);
     }
 }
