8c4f33373cda2d1eaf8d5023806ef7c798d162b4
RinaYamashita <2020m134@kuas.ac.jp>
2020-07-10 11:55:42 +0900

    connect button

--- app/src/main/java/com/example/project03/ColdFoodStyle.java
+++ app/src/main/java/com/example/project03/ColdFoodStyle.java
@@ -2,7 +2,9 @@ package com.example.project03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ColdFoodStyle extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class ColdFoodStyle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cold_food_style);
+    }
+
+    public void goToColdJapaneseActivity(View view) {
+        Intent toColdJapaneseActivity = new Intent(this, ColdJapaneseActivity.class);
+        startActivity(toColdJapaneseActivity);
+    }
+
+    public void goToCold_Western(View view) {
+        Intent toCold_Western = new Intent(this, Cold_Western.class);
+        startActivity(toCold_Western);
+    }
+
+    public void goToCold_Chinese(View view) {
+        Intent toCold_chinese = new Intent(this, Cold_Chinese.class);
+        startActivity(toCold_chinese);
     }
 }
