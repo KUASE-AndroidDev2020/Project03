cba9c294dd44f4c834dff6f49a633a9296839713
Maiparu <2020m121@kuas.ac.jp>
2020-07-31 10:42:42 +0900

    Connected web page in Main

--- app/src/main/java/com/example/project03/MainActivity.java
+++ app/src/main/java/com/example/project03/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.project03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -22,5 +23,10 @@ public class MainActivity extends AppCompatActivity {
     public void goToBreakfastActivity(View view) {
         Intent toBreakfastActivity =new Intent(this, BreakfastActivity.class);
         startActivity(toBreakfastActivity);
+    }
+
+    public void goToLink7(View view) {
+        Intent toLink7 =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.hotpepper.jp/reservation/"));
+        startActivity(toLink7);
     }
 }
